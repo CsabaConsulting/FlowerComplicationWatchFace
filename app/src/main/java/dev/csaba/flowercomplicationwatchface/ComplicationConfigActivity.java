37b8043ee77ca3dda2af3b7cc82fdca6f0189319
MrCsabaToth <csaba.toth.us@gmail.com>
2020-08-19 13:12:20 -0700

    Visual feedback for the color scheme selection logic

--- app/src/main/java/dev/csaba/flowercomplicationwatchface/ComplicationConfigActivity.java
+++ app/src/main/java/dev/csaba/flowercomplicationwatchface/ComplicationConfigActivity.java
@@ -2,9 +2,10 @@ package dev.csaba.flowercomplicationwatchface;
 
 import android.app.Activity;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
+import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
@@ -68,7 +69,7 @@ public class ComplicationConfigActivity extends Activity implements View.OnClick
 
     public static final String COLOR_SCHEME_TAG = "colorScheme";
     private static final int MAX_COLOR_SCHEME = 3;
-    private static final int[] COLOR_SCHEME_BG_RESOURCE_IDS = {
+    private static final int[] COLOR_SELECTOR_BG_RESOURCE_IDS = {
         R.id.red_color_scheme_background1,
         R.id.red_color_scheme_background2,
         R.id.green_color_scheme_background1,
@@ -76,7 +77,7 @@ public class ComplicationConfigActivity extends Activity implements View.OnClick
         R.id.blue_color_scheme_background1,
         R.id.blue_color_scheme_background2
     };
-    private static final int[] COLOR_SCHEME_RESOURCE_IDS = {
+    private static final int[] COLOR_SELECTOR_RESOURCE_IDS = {
         R.id.red_color_scheme1,
         R.id.red_color_scheme2,
         R.id.green_color_scheme1,
@@ -91,6 +92,16 @@ public class ComplicationConfigActivity extends Activity implements View.OnClick
             put("g", 1);
             put("b", 2);
         }};
+    private static final int[] UNSELECTED_RESOURCE_IDS = {
+        R.drawable.red_color_scheme,
+        R.drawable.green_color_scheme,
+        R.drawable.blue_color_scheme,
+    };
+    private static final int[] SELECTED_RESOURCE_IDS = {
+        R.drawable.red_color_scheme_selected,
+        R.drawable.green_color_scheme_selected,
+        R.drawable.blue_color_scheme_selected,
+    };
 
     // Selected complication id by user.
     private int selectedComplicationId;
@@ -104,8 +115,8 @@ public class ComplicationConfigActivity extends Activity implements View.OnClick
     private ImageView[] complicationBackgrounds;
     private ImageButton[] complications;
 
-    private ImageView[] colorSchemeBackgrounds = new ImageView[MAX_COLOR_SCHEME * 2];
-    private ImageButton[] colorSchemes = new ImageButton[MAX_COLOR_SCHEME * 2];
+    private ImageView[] colorSelectorBackgrounds = new ImageView[MAX_COLOR_SCHEME * 2];
+    private ImageButton[] colorSelectors = new ImageButton[MAX_COLOR_SCHEME * 2];
 
     private Drawable defaultAddComplicationDrawable;
 
@@ -121,8 +132,8 @@ public class ComplicationConfigActivity extends Activity implements View.OnClick
 
         selectedComplicationId = -1;
 
-        watchFaceComponentName =
-                new ComponentName(getApplicationContext(), FlowerComplicationWatchFace.class);
+        Context appContext = getApplicationContext();
+        watchFaceComponentName = new ComponentName(appContext, FlowerComplicationWatchFace.class);
 
         complicationBackgrounds = new ImageView[MAX_LOCATION_INDEX + 1];
         complications = new ImageButton[MAX_LOCATION_INDEX + 1];
@@ -141,20 +152,23 @@ public class ComplicationConfigActivity extends Activity implements View.OnClick
         }
 
         colorScheme = getColorScheme();
-        for (int colorSchemeIndex = 0; colorSchemeIndex < MAX_COLOR_SCHEME * 2; colorSchemeIndex++) {
-            colorSchemeBackgrounds[colorSchemeIndex] =
-                    findViewById(COLOR_SCHEME_BG_RESOURCE_IDS[colorSchemeIndex]);
-            if (colorScheme.equals(COLOR_SCHEME_IDS[colorSchemeIndex / 2])) {
-                colorSchemeBackgrounds[colorSchemeIndex].setBackgroundColor(Color.WHITE);
+        for (int selectorIndex = 0; selectorIndex < MAX_COLOR_SCHEME * 2; selectorIndex++) {
+            colorSelectorBackgrounds[selectorIndex] =
+                    findViewById(COLOR_SELECTOR_BG_RESOURCE_IDS[selectorIndex]);
+            int colorSchemeIndex = selectorIndex / 2;
+            if (colorScheme.equals(COLOR_SCHEME_IDS[colorSchemeIndex])) {
+                int selectedResourceId = SELECTED_RESOURCE_IDS[colorSchemeIndex];
+                colorSelectorBackgrounds[selectorIndex].setImageDrawable(
+                        getResources().getDrawable(selectedResourceId, appContext.getTheme()));
             }
-            colorSchemes[colorSchemeIndex] =
-                    findViewById(COLOR_SCHEME_RESOURCE_IDS[colorSchemeIndex]);
-            colorSchemes[colorSchemeIndex].setOnClickListener(this);
+            colorSelectors[selectorIndex] =
+                    findViewById(COLOR_SELECTOR_RESOURCE_IDS[selectorIndex]);
+            colorSelectors[selectorIndex].setOnClickListener(this);
         }
 
         // Initialization of code to retrieve active complication data for the watch face.
         providerInfoRetriever =
-                new ProviderInfoRetriever(getApplicationContext(), Executors.newCachedThreadPool());
+                new ProviderInfoRetriever(appContext, Executors.newCachedThreadPool());
         providerInfoRetriever.init();
 
         retrieveInitialComplicationsData();
@@ -195,22 +209,32 @@ public class ComplicationConfigActivity extends Activity implements View.OnClick
             }
             complicationIndex++;
         }
-        int colorSchemeIndex = 0;
-        for (ImageButton colorSchemeButton: colorSchemes) {
-            if (view.equals(colorSchemeButton)) {
-                if (!colorScheme.equals(COLOR_SCHEME_IDS[colorSchemeIndex / 2])) {
+        int colorSelectorIndex = 0;
+        for (ImageButton colorSelectorButton: colorSelectors) {
+            if (view.equals(colorSelectorButton)) {
+                int colorSchemeIndex = colorSelectorIndex / 2;
+                if (!colorScheme.equals(COLOR_SCHEME_IDS[colorSchemeIndex])) {
                     int currentIndex = COLOR_SCHEME_REVERSE_INDEX.get(colorScheme);
-                    colorSchemeBackgrounds[currentIndex * 2].setBackgroundColor(Color.TRANSPARENT);
-                    colorSchemeBackgrounds[currentIndex * 2 + 1].setBackgroundColor(Color.TRANSPARENT);
-                    colorSchemeBackgrounds[colorSchemeIndex / 2 * 2].setBackgroundColor(Color.WHITE);
-                    colorSchemeBackgrounds[colorSchemeIndex / 2 * 2 + 1].setBackgroundColor(Color.WHITE);
-                    colorScheme = COLOR_SCHEME_IDS[colorSchemeIndex / 2];
-                    setColorScheme(COLOR_SCHEME_IDS[colorSchemeIndex / 2]);
-                    Log.d(TAG, "Set color scheme to " + COLOR_SCHEME_IDS[colorSchemeIndex / 2]);
+                    Resources.Theme appTheme = getApplicationContext().getTheme();
+                    int unselectedResourceId = UNSELECTED_RESOURCE_IDS[currentIndex];
+                    colorSelectorBackgrounds[currentIndex * 2].setImageDrawable(
+                            getResources().getDrawable(unselectedResourceId, appTheme));
+                    colorSelectorBackgrounds[currentIndex * 2 + 1].setImageDrawable(
+                            getResources().getDrawable(unselectedResourceId, appTheme));
+
+                    int selectedResourceId = SELECTED_RESOURCE_IDS[colorSchemeIndex];
+                    colorSelectorBackgrounds[colorSchemeIndex * 2].setImageDrawable(
+                            getResources().getDrawable(selectedResourceId, appTheme));
+                    colorSelectorBackgrounds[colorSchemeIndex * 2 + 1].setImageDrawable(
+                            getResources().getDrawable(selectedResourceId, appTheme));
+
+                    colorScheme = COLOR_SCHEME_IDS[colorSchemeIndex];
+                    setColorScheme(COLOR_SCHEME_IDS[colorSchemeIndex]);
+                    Log.d(TAG, "Set color scheme to " + COLOR_SCHEME_IDS[colorSchemeIndex]);
                 }
                 break;
             }
-            colorSchemeIndex++;
+            colorSelectorIndex++;
         }
     }
 
